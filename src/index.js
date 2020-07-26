6a7458ce2398cb0037873ad58ced7505ce3648c4
Shiraz Solangi <shirazsolangi@gmail.com>
2020-07-26 02:02:42 +0100

    Add serviceworker for pwa support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import { AppRegistry } from "react-native";
 import { App } from "./App";
+import * as serviceWorker from './serviceWorker';
 
 AppRegistry.registerComponent("App", () => App);
 
 AppRegistry.runApplication("App", {
   rootTag: document.getElementById("root")
 });
+
+serviceWorker.unregister();
